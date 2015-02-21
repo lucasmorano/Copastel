66687b2f2a19b5f259ab6cb8aa88d8e2f7c543f6
Lucas Morano <lucas.morano@gmail.com>
2015-02-21 17:58:00 -0300

    Generic Controller

---
+++ src/main/java/com/ppc/controller/GenericController.java
@@ -0,0 +1,44 @@
+package com.ppc.controller;
+
+import com.ppc.service.GenericService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * Created by lucasmorano on 2/21/15.
+ */
+public class GenericController<Document> {
+
+    @Autowired
+    private GenericService<Document> service;
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    public Document create(Document document){
+        return service.create(document);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public List<Document> list(){
+        return service.findAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public void remove(@PathVariable String id){
+        service.remove(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Document retrieve(@PathVariable String id){
+        return service.findById(id);
+    }
+
+}
--- src/main/java/com/ppc/controller/UserController.java
+++ src/main/java/com/ppc/controller/UserController.java
@@ -1,34 +1,14 @@
 package com.ppc.controller;
 
 import com.ppc.document.User;
-import com.ppc.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 /**
 * Created by lucasmorano on 2/21/15.
 */
 @RequestMapping("/User")
 @Controller
-public class UserController  {
+public class UserController extends GenericController<User> {
 
-    @Autowired
-    private UserService userService;
-
-    @RequestMapping(method = RequestMethod.POST)
-    @ResponseBody
-    public User create(User user){
-        return userService.create(new User());
-    }
-
-    @RequestMapping(method = RequestMethod.GET)
-    @ResponseBody
-    public List<User> list(){
-        return userService.findAll();
-    }
 }
